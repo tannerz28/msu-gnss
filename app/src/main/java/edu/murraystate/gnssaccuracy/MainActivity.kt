8801f331732d6a2964926e9ce6c841fe6153c147
BuildTools <unconfigured@null.spigotmc.org>
2019-08-26 21:33:59 -0500

    Initial commit

---
+++ app/src/main/java/edu/murraystate/gnssaccuracy/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.murraystate.gnssaccuracy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
