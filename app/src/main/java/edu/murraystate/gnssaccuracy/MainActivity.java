7ed9388ea563b13a195c69179c2d2fc234eadd11
BuildTools <unconfigured@null.spigotmc.org>
2019-09-14 17:27:27 -0500

    Converted project to Java

---
+++ app/src/main/java/edu/murraystate/gnssaccuracy/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.murraystate.gnssaccuracy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
