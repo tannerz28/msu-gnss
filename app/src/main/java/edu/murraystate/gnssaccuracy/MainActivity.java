4f6d9f5c0a6bd01d4d9dad333f1e9c2ad60bc6d0
BuildTools <unconfigured@null.spigotmc.org>
2019-09-24 22:14:07 -0500

    Use locationManager.requestLocationUpdates to get location updates over time instead of only once. Added GNSS callback, but it's not yet functional. TODO: How to get individual constellations (e.g. Gallileo, GLONASS, etc.)?

--- app/src/main/java/edu/murraystate/gnssaccuracy/MainActivity.java
+++ app/src/main/java/edu/murraystate/gnssaccuracy/MainActivity.java
@@ -5,23 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.GnssStatus;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.util.Log;
 
-import java.text.DecimalFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
     private TextView labelStart;
     private TextView locationGps;
     private TextView locationWifi;
     private TextView errorGps;
     private TextView errorWifi;
     private LocationManager locationManager;
+    GnssStatus.Callback gnssStatusCallback;
+
+    private static final String TAG = "GNSSAccuracy";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +42,45 @@ public class MainActivity extends AppCompatActivity {
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
-        this.requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+        this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+
+        gnssStatusCallback = new GnssStatus.Callback() {
+            @Override
+            public void onStarted() { }
+
+            @Override
+            public void onStopped() { }
+
+            @Override
+            public void onFirstFix(int millis) { }
+
+            @Override
+            public void onSatelliteStatusChanged(GnssStatus status) {
+                Log.v(TAG, "GNSS Status: " + status.getSatelliteCount() + " satellites.");
+            }
+        };
     }
 
     public void onStartClick(View view) {
         setStartLabel();
-        getGpsLocation();
-        getWifiLocation();
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        locationManager.registerGnssStatusCallback(gnssStatusCallback);
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 1000, 0,this
+        );
+        locationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER, 1000, 0, this
+        );
     }
 
     public void setStartLabel() {
@@ -51,42 +89,31 @@ public class MainActivity extends AppCompatActivity {
         labelStart.setText(labelText);
     }
 
-    public void getGpsLocation() {
-        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    Activity#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for Activity#requestPermissions for more details.
-            return;
-        }
-
-        Location gpsLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        locationGps.setText(formatMeasurement(getLocationMeasurement(gpsLocation)));
-    }
-
-    public void getWifiLocation() {
-        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    Activity#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for Activity#requestPermissions for more details.
-            return;
-        }
-        Location wifiLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        locationWifi.setText(formatMeasurement(getLocationMeasurement(wifiLocation)));
-    }
-
     private double getLocationMeasurement(Location location) {
-        return Math.abs(location.getLongitude()) + Math.abs(location.getLatitude()) + Math.abs(location.getTime());
+        return Math.abs(location.getLongitude()) + Math.abs(location.getLatitude()) + Math.abs(location.getAltitude());
     }
 
-    private String formatMeasurement(double measurement) {
-        return new DecimalFormat("#.######").format(measurement);
+    @Override
+    public void onLocationChanged(Location location) {
+        switch (location.getProvider()) {
+            case LocationManager.NETWORK_PROVIDER:
+                locationWifi.setText(Double.toString(getLocationMeasurement(location)));
+                break;
+            case LocationManager.GPS_PROVIDER:
+                locationGps.setText(Double.toString(getLocationMeasurement(location)));
+                break;
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
     }
 }
